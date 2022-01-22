27bc24bf028fb0d3f99cf3f003f7e5039c051f89
ErasunN <nicoerasun98@gmail.com>
2022-01-22 17:05:21 -0300

    Fix de error de tipeo

--- js/components/Contador/Contador.js
+++ js/components/Contador/Contador.js
@@ -12,7 +12,7 @@ const Contador = () => {
 
     return (
         <div id="contador">
-            <h1 className={cuenta <= 0 ? "negativo" : "positivo"}>Counter: {contador}</h1>
+            <h1 className={contador <= 0 ? "negativo" : "positivo"}>Counter: {contador}</h1>
             <hr />
             <button className="boton-contador" onClick={aumentarContador}>+</button>
             <button className="boton-contador" onClick={disminuirContador}>-</button>
